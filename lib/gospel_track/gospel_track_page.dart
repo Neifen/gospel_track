08ed3ebfbc98931830d403d4b523b68ab487ef67
Neifen <neifen.b@gmailcom>
2021-10-13 09:22:42 -1000

    - has a bottom drawer thing now

--- lib/gospel_track/gospel_track_page.dart
+++ lib/gospel_track/gospel_track_page.dart
@@ -5,7 +5,7 @@ import 'package:gospel_track/gospel_track/page_number.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
-class GospelTrackPage extends StatelessWidget {
+class GospelTrackPage extends StatefulWidget {
   static const List<String> _imgList = [
     'pictures/crappy/1_5finger.jpg',
     'pictures/crappy/2_5finger.jpg',
@@ -18,9 +18,14 @@ class GospelTrackPage extends StatelessWidget {
   const GospelTrackPage({Key? key}) : super(key: key);
 
   @override
+  State<GospelTrackPage> createState() => _GospelTrackPageState();
+}
+
+class _GospelTrackPageState extends State<GospelTrackPage> {
+  @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance!.addPostFrameCallback((_) {
-      for (var imageUrl in _imgList) {
+      for (var imageUrl in GospelTrackPage._imgList) {
         precacheImage(AssetImage(imageUrl), context);
       }
     });
@@ -28,36 +33,113 @@ class GospelTrackPage extends StatelessWidget {
     final double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: GestureDetector(
-        onVerticalDragUpdate: (details) {
-          if (details.primaryDelta != null && details.primaryDelta! < -9) {
-            Navigator.push(
-                context,
-                PageTransition(
-                    type: PageTransitionType.bottomToTop,
-                    child: const InfoGospelPage()));
+        onVerticalDragEnd: (details) {
+          if (details.primaryVelocity != null) {
+            changeInfoHeight(details.primaryVelocity!);
           }
         },
-        child: CarouselSlider(
-          options: CarouselOptions(
-              initialPage: context.read<PageNumber>().currentPage,
-              enableInfiniteScroll: false,
-              height: height,
-              viewportFraction: 1.0,
-              enlargeCenterPage: false,
-              onPageChanged: (index, _) =>
-                  context.read<PageNumber>().currentPage = index),
-          items: _imgList
-              .map((item) => Container(
-                  color: Colors.white,
-                  child: Center(
-                      child: Image.asset(
-                    item,
-                    fit: BoxFit.fitWidth,
-                    height: height,
-                  ))))
-              .toList(),
+        onVerticalDragUpdate: (details) {
+          if (details.primaryDelta != null) {
+            prepareInfoHeight(details.primaryDelta!);
+          }
+        },
+        child: Stack(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                  initialPage: context.read<PageNumber>().currentPage,
+                  enableInfiniteScroll: false,
+                  height: height,
+                  viewportFraction: 1.0,
+                  enlargeCenterPage: false,
+                  onPageChanged: (index, _) =>
+                      context.read<PageNumber>().currentPage = index),
+              items: GospelTrackPage._imgList
+                  .map((item) => Container(
+                      color: Colors.white,
+                      child: Center(
+                          child: Image.asset(
+                        item,
+                        fit: BoxFit.fitWidth,
+                        height: height,
+                      ))))
+                  .toList(),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Spacer(),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(0),
+                        topRight: Radius.circular(0)),
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                  ),
+                  height: _infoHeight,
+                  child: const InfoGospelPage(),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
+  }
+
+  var _infoHeight = 0.0;
+  var _heightOnTouchUp = 0.0;
+  changeInfoHeight(double primaryVelocity) {
+    const minHeight = 0.0;
+    final maxHeight = MediaQuery.of(context).size.height;
+    setState(() {
+      if (primaryVelocity < -800) {
+        _infoHeight = maxHeight;
+        return;
+      } else if (primaryVelocity > 800) {
+        _infoHeight = minHeight;
+        return;
+      }
+      _infoHeight = _heightOnTouchUp;
+    });
+  }
+
+  prepareInfoHeight(double delta) {
+    const minHeight = 0.0;
+    final maxHeight = MediaQuery.of(context).size.height;
+    final upThreshhold = MediaQuery.of(context).size.height * 0.35;
+    final downThreshhold = MediaQuery.of(context).size.height * 0.75;
+
+    var newHeight = _infoHeight - delta;
+
+    //don't go over or under
+    if (newHeight < minHeight || newHeight > maxHeight) {
+      return;
+    }
+
+    //speed is most important first
+
+    //is it moving up or down.
+    bool up = newHeight > _infoHeight;
+    if (up) {
+      if (newHeight > upThreshhold) {
+        _heightOnTouchUp = maxHeight;
+      } else {
+        _heightOnTouchUp = minHeight;
+      }
+    } else {
+      if (newHeight < downThreshhold) {
+        _heightOnTouchUp = minHeight;
+      } else {
+        _heightOnTouchUp = maxHeight;
+      }
+    }
+
+    setState(() {
+      _infoHeight = newHeight;
+    });
   }
 }
--- lib/gospel_track/info_gospel_page.dart
+++ lib/gospel_track/info_gospel_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gospel_track/gospel_track/gospel_info.dart';
-import 'package:gospel_track/gospel_track/gospel_track_page.dart';
 import 'package:gospel_track/gospel_track/page_number.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
 class InfoGospelPage extends StatelessWidget {
@@ -28,38 +26,11 @@ class InfoGospelPage extends StatelessWidget {
       ));
     }
 
-    return GestureDetector(
-      onVerticalDragUpdate: (details) {
-        if (details.primaryDelta != null && details.primaryDelta! > 9) {
-          Navigator.pushAndRemoveUntil(
-              context,
-              PageTransition(
-                  type: PageTransitionType.topToBottom,
-                  child: const GospelTrackPage()),
-              (route) => route.isFirst);
-        }
-      },
-      onHorizontalDragUpdate: (details) {
-        if (details.primaryDelta != null && details.primaryDelta! < -9) {
-          context.read<PageNumber>().nextPage();
-        } else if (details.primaryDelta != null && details.primaryDelta! > 9) {
-          context.read<PageNumber>().lastPage();
-        }
-      },
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          automaticallyImplyLeading: false,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-              children: columnList,
-            ),
-          ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: columnList,
         ),
       ),
     );
